cbb714395ce633dcda2e83ff667289b09d436017
Enora Rice <erice@CFMBook1.lan>
2021-07-01 10:54:32 -0400

    first steps

---
+++ mentor_classifier/mentor_classifier/ner.py
@@ -0,0 +1,8 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+sentence ="My name is EMC Clint Anderson, that's Electrician's Mate Clinton Anderson. I was born in Los Angeles, California. I was raised there most of my life and I graduated from high school there. A couple of years after graduating from high school, then I joined the United States Navy. I was an Electrician's Mate for eight years. I served on an aircraft carrier. We went on many deployments. A deployment is when you go to war, you fight. We fought in the Iraq war. I went on three deployments and it was a really great time in my life. I had a lot of fun. At the end of the eight years, I decided that the Navy wasn't quite a career for me. So, I got out of the Navy. I started using the education benefits that we received and I started going to the University of California at Berkeley. I was majoring in computer science and afterwards, I started getting my master's degree from the University of Southern California. I also had a job at the Institute for Creative Technologies. It's been a lot of fun, this whole time. Thanks to the Navy."
+doc = nlp(sentence)
+print(doc.text)
+for x in doc.ents:
+    print(x.text + ": " + x.label_)
