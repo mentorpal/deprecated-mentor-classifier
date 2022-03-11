99ac0862d2aa89e132823277941213cddd023e54
aaronshiel <ashiel408@gmail.com>
2022-03-11 12:52:21 -0800

    additional blueprint format added to route

--- mentor_classifier_api/src/mentor_classifier_api/blueprints/followups.py
+++ mentor_classifier_api/src/mentor_classifier_api/blueprints/followups.py
@@ -22,6 +22,7 @@ def get_auth_headers() -> Dict[str, str]:
     )
 
 
+@followups_blueprint.route("followups/category/<category>/<mentor>", methods=["POST"])
 @followups_blueprint.route("followups/category/<category>/<mentor>/", methods=["POST"])
 def followup(category: str, mentor: str):
     data = generate_followups(
